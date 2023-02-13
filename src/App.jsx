0e1e3085b566bb20dcaad024b475faea8716b672
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-02-13 16:21:36 +0300

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { ToastContainer } from 'react-toastify';
 import Loader from 'shared/components/Loader/Loader';
 import Layout from 'modules/Layout/Layout';
 
-// const Navbar = lazy(() => import('./modules/Navbar/Navbar'));
+const Navbar = lazy(() => import('./modules/Navbar/Navbar'));
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const MoviePage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
 const MovieDetailsPage = lazy(() =>
@@ -18,21 +18,19 @@ export const App = () => {
   return (
     <>
       <BrowserRouter>
-        {/* <Navbar /> */}
+        <Navbar />
         <Suspense fallback={<Loader />}>
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<HomePage />} />
-
-              <Route path="movies" element={<MoviePage />}>
-                <Route  path="movies/:movieId" element={<MovieDetailsPage />}>
-                  <Route path="cast" element={<CastPage />} />
-                  <Route path="reviews" element={<ReviewsPage />} />
-                </Route>
+              <Route path="movies" element={<MoviePage />} />
+              <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+                <Route path="cast" element={<CastPage />} />
+                <Route path="reviews" element={<ReviewsPage />} />
               </Route>
-
             </Route>
-
+            <Route path="*" element={<Navigate to="/" />} />
+            
             {/* <Route path="/" element={<HomePage />} />
             <Route path="/movies" element={<MoviePage />} />
             <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
--- src/modules/Layout/Layout.jsx
+++ src/modules/Layout/Layout.jsx
@@ -1,17 +1,10 @@
 import { Outlet } from 'react-router-dom';
 
-import Navbar from 'modules/Navbar/Navbar';
-
 const Layout = () => {
   return (
-    <>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <main>
+      <Outlet />
+    </main>
   );
 };
 
